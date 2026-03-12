39b84a3081cb4e521f35e6eee837a16f1829c357
silkm <michael.silk@populationgenomics.org.au>
2026-03-13 10:03:42 +1100

    add plink qc job

---
+++ src/popgen_genotyping/jobs/plink2_qc_job.py
@@ -0,0 +1,91 @@
+"""
+Plink2 outputs from pgen/pvar
+"""
+
+from typing import TYPE_CHECKING
+
+from cpg_utils.config import config_retrieve
+from cpg_utils.hail_batch import get_batch
+
+from popgen_genotyping.utils import register_job
+
+if TYPE_CHECKING:
+    from hailtop.batch.job import Job
+
+
+def run_plink2_qc(
+    pgen_path: str,
+    outputs_path: str,
+    job_name: str = 'plink2_qc',
+) -> 'Job':
+    """
+    Generate plink2 QC outputs from pgen/pvar files.
+
+    Args:
+        pgen_path (str): Cloud path to the input plink pgen, pvar file path implied at same loc.
+        outputs_path (str): Cloud path for the QC output files.
+        job_name (str): Name for the Hail Batch job.
+
+    Returns:
+        BashJob: The queued Hail Batch job.
+
+    """
+    b = get_batch()
+    j = register_job(
+        batch=b,
+        job_name=job_name,
+        config_path=['popgen_genotyping', 'plink_qc'],
+        image=config_retrieve(['workflow', 'plink_image']),  # TODO confirm whether plink image has plink2
+        default_cpu=2,
+        default_storage='50G',
+    )
+
+    # Read inputs into local env. Plink2 requires 3 files in the same location,
+    # and the command requires the path prefix to the files (ie. without the
+    # file extension).
+    pvar_path = pgen_path.replace('.pgen', '.pvar')
+    psam_path = pgen_path.replace('.pgen', '.psam')
+
+    # Read inputs to local env
+    pgen_files = b.read_input_group(
+        pgen=pgen_path,
+        pvar=pvar_path,
+        psam=psam_path,
+    )
+
+    pgen_prefix = pgen_files.pgen.removesuffix('.pgen')
+
+    # Define output files
+    j.declare_resource_group(
+        plink_qc_outputs={
+            'smiss': '{root}.smiss',
+            'vmiss': '{root}.vmiss',
+            'afreq': '{root}.afreq',
+            'hwe': '{root}.hwe',
+            'het': '{root}.het',
+            'sexcheck': '{root}.sexcheck',
+            'kin': '{root}.kin',
+        },
+    )
+
+    j.command(
+        f"""
+        set -ex
+        plink2 --pfile {pgen_prefix} \\
+            --missing \\
+            --freq \\
+            --hardy \\
+            --het \\
+            --check-sex max-female-xf=0.25 \
+                min-male-xf=0.75 \
+                max-female-ycount=100 \
+                min-male-ycount=1000 \
+                cols='status,pedsex,xf,ycount,yrate' \\
+            --make-king-table \\
+            --out {j.plink_qc_outputs.root}
+        """
+    )
+
+    b.write_output(j.plink_qc_outputs, outputs_path)
+
+    return j
