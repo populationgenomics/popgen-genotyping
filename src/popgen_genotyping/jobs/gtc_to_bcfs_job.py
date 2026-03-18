1e683082ea070b25bd1c49f4fae544bad89a388f
Alex Stuckey <alex.stuckey@populationgenomics.org.au>
2026-03-19 12:14:48 +1300

    more fixes

--- src/popgen_genotyping/jobs/gtc_to_bcfs_job.py
+++ src/popgen_genotyping/jobs/gtc_to_bcfs_job.py
@@ -19,7 +19,9 @@ def run_gtc_to_bcfs(
     gtc_paths: list[str],
     sample_mapping: dict[str, str],
     output_heavy_bcf_path: str,
+    output_heavy_bcf_index_path: str,
     output_light_bcf_path: str,
+    output_light_bcf_index_path: str,
     output_metadata_path: str,
     bpm_manifest_path: str,
     egt_cluster_path: str,
@@ -100,8 +102,10 @@ EOF
         """
     )
 
-    b.write_output(j.heavy_bcf, output_heavy_bcf_path.replace('.bcf', ''))
-    b.write_output(j.light_bcf, output_light_bcf_path.replace('.bcf', ''))
+    b.write_output(j.heavy_bcf, output_heavy_bcf_path)
+    b.write_output(j.heavy_bcf_index, output_heavy_bcf_path + '.csi')
+    b.write_output(j.light_bcf, output_light_bcf_path)
+    b.write_output(j.light_bcf_index, output_light_bcf_path + '.csi')
     b.write_output(j.metadata_tsv, output_metadata_path)
 
     return j
