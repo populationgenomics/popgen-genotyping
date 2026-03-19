e38e8c2fc5aa480cb1acbef60116612a2305683b
Alex Stuckey <13140057+Alexander-Stuckey@users.noreply.github.com>
2026-03-19 16:38:36 +1300

    hopefully finally fix hail batch syntax (#15)

--- src/popgen_genotyping/jobs/gtc_to_bcfs_job.py
+++ src/popgen_genotyping/jobs/gtc_to_bcfs_job.py
@@ -70,8 +70,8 @@ def run_gtc_to_bcfs(
 
     # Outputs
     j.declare_resource_group(
-        heavy_bcf={'bcf': '{root}.bcf', 'index': '{root}.bcf.csi'},
-        light_bcf={'bcf': '{root}.bcf', 'index': '{root}.bcf.csi'},
+        heavy_bcf={'bcf': '{root}.bcf', 'bcf.csi': '{root}.bcf.csi'},
+        light_bcf={'bcf': '{root}.bcf', 'bcf.csi': '{root}.bcf.csi'},
         metadata_tsv={'tsv': '{root}.tsv'},
     )
 
@@ -109,6 +109,6 @@ EOF
 
     b.write_output(j.heavy_bcf, str(output_heavy_bcf_path).replace('.bcf', ''))
     b.write_output(j.light_bcf, str(output_light_bcf_path).replace('.bcf', ''))
-    b.write_output(j.metadata_tsv, output_metadata_path)
+    b.write_output(j.metadata_tsv, str(output_metadata_path).replace('.tsv', ''))
 
     return j
